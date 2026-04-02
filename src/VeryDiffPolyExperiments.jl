148274e387c0041d50dd7fcbb1b81f5732645488
Philipp Kern <philipp.kern@kit.edu>
2026-04-02 16:59:17 +0200

    hot fix to make update to new verydiff work

--- src/VeryDiffPolyExperiments.jl
+++ src/VeryDiffPolyExperiments.jl
@@ -4,7 +4,7 @@ using VeryDiff, VNNLib, LinearAlgebra
 using Tar, CodecZlib, JLD2, JSON3, CSV, Dates
 
 import VeryDiff: approximate_polynomial, approximate_polynomial_iterative, approximate_polynomial_abcrown, approximate_polynomial_iterative_sampling,
-                 extract_approximation_domain, LayeredModel, to_layered_model
+                 extract_approximation_domain
 
 const OXP = VNNLib.OnnxParser
 
@@ -22,15 +22,15 @@ end
 include("utils.jl")
 include("verydiff_patch/patch.jl")
 include("fhe_export/json_export.jl")
-include("generate_nns/generate_nns.jl")
-include("generate_nns/generate_nns_mnist.jl")
-include("generate_nns/generate_nns_heloc.jl")
-include("generate_nns/generate_nns_har.jl")
-include("verify_nns/verify_eps_equivalence.jl")
-include("verify_nns/verify_eps_equivalence_mnist.jl")
-include("verify_nns/verify_eps_equivalence_heloc.jl")
-include("verify_nns/verify_eps_equivalence_har.jl")
-include("gelu/generate_nns_gelu.jl")
+#include("generate_nns/generate_nns.jl")
+#include("generate_nns/generate_nns_mnist.jl")
+#include("generate_nns/generate_nns_heloc.jl")
+#include("generate_nns/generate_nns_har.jl")
+#include("verify_nns/verify_eps_equivalence.jl")
+#include("verify_nns/verify_eps_equivalence_mnist.jl")
+#include("verify_nns/verify_eps_equivalence_heloc.jl")
+#include("verify_nns/verify_eps_equivalence_har.jl")
+#include("gelu/generate_nns_gelu.jl")
 
 
 
--- src/fhe_export/json_export.jl
+++ src/fhe_export/json_export.jl
@@ -1,26 +1,44 @@
 
 
 """
-Adds description of each layer in a LayeredModel as a tuple to a list.
+    `convert_to_list_of_tuples(nn_layers)`
 
-Each tuple contains the layer type as a string and the necessary parameters. E.g.:
+Adds description of each layer as a tuple to a list.
 
-[("linear", W1, b1), ("chebyshev", coeffs1, l1, u1), ...]
+Each tuple contains the layer type as a string, the input ids and output ids and the necessary parameters. E.g.:
+
+[("linear", [0], [1], W1, b1), ("chebyshev", [1], [2], coeffs1, l1, u1), ...]
 
 args:
-- net - the LayeredModel to convert.
+- nn_layers - the list of layers to convert.
 """
-function convert_to_list_of_tuples(net::LayeredModel)
+function convert_to_list_of_tuples(nn_layers)
     layers = []
-    for layer in net.layers
+    for layer in nn_layers
         if layer isa OXP.ONNXLinear
             push!(layers, ("linear", layer.dense.weight, layer.dense.bias))
+        elseif layer isa OXP.ONNXConv
+            kernel_size = size(layer.conv.weight)[1:2]
+            input_channels = size(layer.conv.weight)[3]
+            output_channels = size(layer.conv.weight)[4]
+            push!(layers, ("conv", kernel_size, input_channels, output_channels, layer.conv.weight, layer.conv.bias, layer.conv.stride, layer.conv.pad))
+        elseif layer isa OXP.ONNXBatchNorm
+            # stores scale, bias, mean, variance (not standard-deviation), epsilon for numerical stability and number of channels
+            push!(layers, ("batchnorm", layer.batchnorm.γ, layer.batchnorm.β, layer.batchnorm.μ, layer.batchnorm.σ², layer.batchnorm.ϵ, layer.batchnorm.chs))
+        elseif layer isa OXP.ONNXReshape
+            if sum(layer.shape .!= 1) == 1
+                push!(layers, ("flatten"))
+            else
+                push!(layers, ("reshape", layer.shape))
+            end
+        elseif layer isa OXP.ONNXFlatten
+            push!(layers, ("flatten"))
         elseif layer isa VeryDiff.ONNXChebyshevPoly
             push!(layers, ("chebyshev", layer.coeffs, layer.l, layer.u))
         elseif layer isa OXP.ONNXRelu
-            push!(layers, ("relu",))
+            push!(layers, ("relu"))
         elseif layer isa OXP.ONNXGelu
-            push!(layers, ("gelu",))
+            push!(layers, ("gelu"))
         else
             error("Unsupported layer type: $(typeof(layer))")
         end
@@ -50,33 +68,73 @@ function merge_normalization_into_dense(layer_lin::OXP.ONNXLinear, layer_poly::V
 
     n = length(l)
     return OXP.ONNXLinear(layer_lin.inputs, layer_lin.outputs, layer_lin.name, Ŵ, b̂, double_precision=true), 
-           VeryDiff.ONNXChebyshevPoly(layer_poly.inputs, layer_poly.outputs, layer_poly.name, layer_poly.coeffs, -ones(N, n), ones(N, n))
+           VeryDiff.ONNXChebyshevPoly(layer_poly.inputs, layer_poly.outputs, layer_poly.name, layer_poly.coeffs, -ones(N, n), ones(N, n), layer_poly.ϵ)
+end
+
+function merge_normalization_into_dense(layer_lin::OXP.ONNXConv, layer_poly::VeryDiff.ONNXChebyshevPoly)
+    # hard to merge bounds into conv layer because every neuron might have different bounds, but conv kernels are applied to all of them.
+    # TODO: We can merge, if we only have a single polynomial per layer!
+    return layer_lin, layer_poly
+end
+
+function merge_normalization_into_dense(layer1, layer2)
+    # fallback for all other layers
+    return layer1, layer2
 end
 
 
-function merge_normalization_into_dense(net::LayeredModel{S}) where S
-    layers = net.layers
+"""
+    `merge_normalization_into_dense(layers)`
+
+Merges normalization of Chebyshev approximation bounds from [l, u] to [-1, 1] into the weights and biases of the preceeding linear layer.
+This is done for all chebyshev layers in the network.
+
+Merging is not possible for convolutional layers, because every neuron might have different bounds, but conv kernels are applied to all of them.
+
+args:
+- layers - list of layers in the network, must be topologically sorted.
+
+returns:
+- new list of layers with normalization merged into linear parameters and [-1, 1] for the chebyshev approximation bounds (where merging was possible).
+"""
+function merge_normalization_into_dense(layers::Vector{VeryDiff.Definitions.OnnxLayer{S}}) where {S}
     new_layers = Vector{OXP.Node{S}}(undef, length(layers))
 
-    for i in 1:2:length(layers) - 1
-        layer_lin = layers[i]
-        layer_poly = layers[i + 1]
-        layer_lin_new, layer_poly_new = merge_normalization_into_dense(layer_lin, layer_poly)
-        new_layers[i] = layer_lin_new
-        new_layers[i + 1] = layer_poly_new
+    output_map = Dict{Int, Int}() # maps output id to layer that produces that output
+    for (i, layer) in enumerate(layers)
+        for output_id in layer.output_ids
+            output_map[output_id] = i
+        end
     end
 
-    if isodd(length(layers))
-        new_layers[end] = layers[end]
+    for (i, l) in enumerate(layers)
+        if l.node isa VeryDiff.ONNXChebyshevPoly
+            # find preceding layer
+            input_id = l.input_ids[1] # assuming single input for chebyshev layer
+            if haskey(output_map, input_id)
+                preceding_layer_idx = output_map[input_id]
+                preceding_layer = layers[preceding_layer_idx]
+                new_lin, new_poly = merge_normalization_into_dense(preceding_layer.node, l.node)
+                new_layers[preceding_layer_idx] = new_lin
+                new_layers[preceding_layer_idx + 1] = new_poly
+            else
+                error("No preceding layer found for chebyshev layer with input id: ", input_id)
+            end
+        else
+            # because we assume topologically sorted layers, linear layers may be added here, but will then
+            # be overwritten if the following layer is a chebyshev layer. All other layers are not changed.
+            new_layers[i] = l.node
+        end
     end
 
-    return LayeredModel(new_layers)
+    return new_layers
 end
 
 
-function export2json(net::LayeredModel{S}, outfile) where S
-    net = merge_normalization_into_dense(net)
-    list_of_tuples = convert_to_list_of_tuples(net)
+function export2json(net::OnnxNet{LayerIdT,NShapeIn,NShapeOut}, outfile) where {LayerIdT,NShapeIn,NShapeOut}
+    layers, _ = VeryDiff.Definitions.sort_network(net)
+    layers = merge_normalization_into_dense(layers)
+    list_of_tuples = convert_to_list_of_tuples(layers)
     s = sprint(JSON3.pretty, list_of_tuples)
     write(outfile, s)
 end
