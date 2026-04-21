a3ad484202347a30ea6d8dfd38d3a0d507bc92f3
Philipp Kern <philipp.kern@kit.edu>
2026-04-21 22:09:23 +0200

    add warmup run

--- experiment_scripts/run_all.jl
+++ experiment_scripts/run_all.jl
@@ -3,4 +3,6 @@ using VeryDiffPolyExperiments, VeryDiff
 
 VeryDiff.ALMOST_ZERO_LEADING_COEFF_WARNING[] = false
 
+VeryDiffPolyExperiments.warmup()
+
 VeryDiffPolyExperiments.run_mnist_experiment(degrees=20:20:100, n_threads=Threads.nthreads())
