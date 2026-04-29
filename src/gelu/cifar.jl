0b04b3b8217746e60faa3c25499010a28b2e643c
Philipp Kern <philipp.kern@kit.edu>
2026-04-29 18:39:36 +0200

    rerun relu cifar networks

--- src/gelu/cifar.jl
+++ src/gelu/cifar.jl
@@ -78,10 +78,10 @@ end
 
 function generate_cifar_large_scale(;degrees=[119, 247])
     onnx_paths = [
-        joinpath(@__DIR__, "..", "..", "networks", "cifar", "best_model_bn_4_0.0001l1.onnx"),
-        joinpath(@__DIR__, "..", "..", "networks", "cifar", "best_model_bn_8_0.0001l1_no_pad.onnx"),
-        joinpath(@__DIR__, "..", "..", "networks", "cifar", "models_relu", "best_model_relu_bn_4_0.0001l1.onnx"),
-        joinpath(@__DIR__, "..", "..", "networks", "cifar", "models_relu", "best_model_relu_bn_8_0.0001l1.onnx")
+        #joinpath(@__DIR__, "..", "..", "networks", "cifar", "best_model_bn_4_0.0001l1.onnx"),
+        #joinpath(@__DIR__, "..", "..", "networks", "cifar", "best_model_bn_8_0.0001l1_no_pad.onnx"),
+        joinpath(@__DIR__, "..", "..", "networks", "cifar", "models_relu", "best_model_relu_bn_4_0.0001l1_new.onnx"),
+        joinpath(@__DIR__, "..", "..", "networks", "cifar", "models_relu", "best_model_relu_bn_8_0.0001l1_new.onnx")
     ]
 
     for onnx_path in onnx_paths
