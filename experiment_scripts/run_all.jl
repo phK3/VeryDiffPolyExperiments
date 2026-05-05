1bab035e08a0a07abd1836e27c7a11e75e47153a
Philipp Kern <philipp.kern@kit.edu>
2026-05-05 08:57:43 +0200

    need to run mnist sampling again

--- experiment_scripts/run_all.jl
+++ experiment_scripts/run_all.jl
@@ -5,7 +5,7 @@ VeryDiff.ALMOST_ZERO_LEADING_COEFF_WARNING[] = false
 
 VeryDiffPolyExperiments.warmup()
 
-# VeryDiffPolyExperiments.run_mnist_experiment(degrees=20:20:100, n_threads=Threads.nthreads())
+VeryDiffPolyExperiments.run_mnist_experiment(degrees=20:20:100, n_threads=Threads.nthreads())
 
 VeryDiffPolyExperiments.run_collins_experiment(degrees=20:20:160, n_threads=Threads.nthreads())
 
